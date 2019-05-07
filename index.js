7ca4625474735c69b494f174fa8a11ee1ebf685f
Jenya Gul <gul.jenya@gmail.com>
2019-05-07 22:08:21 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = function timing (keyframes, t) {
+  const _keyframes = [[0, 0], ...keyframes, [1, 1]]
+  const current = _keyframes.findIndex(frame => frame[0] >= t)
+  const currentFrame = _keyframes[current]
+  const prevFrame = _keyframes[current - 1]
+  const x = prevFrame ? prevFrame[0] : 0
+  const xD = currentFrame[0] - x
+  const time = (t - x) / xD
+  const yD = currentFrame[1] - (prevFrame ? prevFrame[1] :  0)
+  const prevent = prevFrame ? prevFrame[1] : 0
+  return prevent + yD * time
+}
